4fbec0f376d9d6724b3298e9f486982c17679e12
choidoa-git <pearl097@naver.com>
2020-11-10 00:25:16 +0900

    TIL: DAO DTO 작성

--- doa/lecture02/project/src/main/java/dao/TodoDao.java
+++ doa/lecture02/project/src/main/java/dao/TodoDao.java
@@ -1,5 +1,60 @@
 package dao;
 
-public class TodoDao {
+import java.sql.*;
 
+public class TodoDao {
+    String url = "jdbc:mysql://localhost:3306/todolist";
+    String user = "connect";
+    String passwd = "1234";
+    String driver = "com.mysql.jdbc.Driver";
+
+    public int TodoAdd() {
+        Connection conn = null; // DB연결 객체
+        PreparedStatement ps = null;    // SQL문을 디비에 전달
+        String SQLQuery = "insert into todo(title, name, sequence) values(?,?,?)";
+
+
+        // 예외처리
+        try {
+            Class.forName(driver);
+            conn = DriverManager.getConnection(url, user, passwd);
+            System.out.println("Connection ok");
+            ps = conn.prepareStatement(SQLQuery);
+
+            ps.setString(1, "Test");
+            ps.setString(2, "test");
+            ps.setInt(3, 1234);
+
+            int rs = ps.executeUpdate();
+            System.out.println(rs);
+        } catch (ClassNotFoundException ex) {
+            System.err.println(ex.getMessage() + " :: Driver Loading 실패");
+            ex.printStackTrace();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage() + " :: Connection 실패 " + url + "/" + user + "/" + passwd);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+//            if (rs != null) {
+//                try {
+//                    rs.close();
+//                } catch (SQLException e) {
+//                    e.printStackTrace();
+//                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
 }
+
