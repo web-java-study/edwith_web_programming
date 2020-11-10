0a246e3cf608fbec4629bcb3492663a9be61e03a
choidoa-git <pearl097@naver.com>
2020-11-11 01:19:00 +0900

    TIL: DAO, DTO 작성중

--- doa/lecture02/project/src/main/java/dao/TodoDao.java
+++ doa/lecture02/project/src/main/java/dao/TodoDao.java
@@ -1,5 +1,30 @@
 package dao;
 
-public class TodoDao {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.*;
 
+import java.util.ArrayList;
+import dbConnect.DBConn;
+
+public class TodoDao {
+	PreparedStatement ps = null;
+	
+	public static void main(String[] args) {
+	
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+	
+		DBConn test = new DBConn();
+		System.out.println(test);
+		
+		String SQLQuery = "insert into todo(title, name, sequence) values(?,?,?)";
+
+		// 20201111
+
+	}
 }
