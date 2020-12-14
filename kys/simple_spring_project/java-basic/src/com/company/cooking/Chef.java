b9efc61b110f83adc8cf7caf368f1c39311963aa
강용석 <kangyongsuek@gmail.com>
2020-12-14 22:33:39 +0900

    add: 요리예제로 자바 실습코드 작성

--- kys/simple_spring_project/java-basic/src/com/company/cooking/Chef.java
+++ kys/simple_spring_project/java-basic/src/com/company/cooking/Chef.java
@@ -1,2 +1,10 @@
-package com.company.cooking;public class Chef {
+package com.company.cooking;
+
+public class Chef {
+    // 요리사는 요리를한다
+    String cooking;
+
+    public void setCooking(String cooking) {
+        this.cooking = cooking;
+    }
 }
--- kys/simple_spring_project/java-basic/src/com/company/cooking/Cooking.java
+++ kys/simple_spring_project/java-basic/src/com/company/cooking/Cooking.java
@@ -1,2 +1,10 @@
-package com.company.cooking;public class Cooking {
+package com.company.cooking;
+
+public class Cooking extends Chef {
+ // 요리는 여러가지가 있다.
+ public static void main(String[] args) {
+     Cooking cooking = new Cooking();
+     cooking.setCooking("Pasta");
+     System.out.println(cooking.cooking);
+ }
 }
