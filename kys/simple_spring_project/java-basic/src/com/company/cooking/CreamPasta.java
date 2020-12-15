c477e4d5ae256e34df8c62af64764abe8436b1f5
강용석 <kangyongsuek@gmail.com>
2020-12-15 22:43:49 +0900

    add: 크림파스타 클래스 추가

---
+++ kys/simple_spring_project/java-basic/src/com/company/cooking/CreamPasta.java
@@ -0,0 +1,12 @@
+package com.company.cooking;
+
+public class CreamPasta {
+    int bacon = 3;
+    String milk = "3cup";
+    String garlic = "1T";
+    float greenOnion = 0.5f;
+    String oliveOil = "0.5cup";
+    String pastaNoodles = "한줌";
+    String salt = "1T";
+    float onion = 0.25f;
+}
